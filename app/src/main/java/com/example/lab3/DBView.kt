6a687dc819d321ee0ba7ee35aa1a542ef197171e
platon.trunov@gmail.com <platon.trunov@gmail.com>
2021-05-23 01:35:02 +0700

    Уведомления вкл и выкл

--- app/src/main/java/com/example/lab3/DBView.kt
+++ app/src/main/java/com/example/lab3/DBView.kt
@@ -1,9 +1,14 @@
 package com.example.lab3
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.core.app.NotificationCompat
 import com.example.lab3.data.Groupmates
 import kotlin.reflect.typeOf
 
@@ -18,9 +23,25 @@ class DBView : AppCompatActivity() {
 
         var adapter:ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,groupmate)
         listView.adapter = adapter
+        notification()
 
+    }
 
-
+    fun notification(){
+        val groupmateLen = DBConnect.db.groupmatesDAO().getAllGroupmates().size
+        val NOTIFY_ID: Int = 100;
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val contentIntent = PendingIntent.getActivity(
+            this,
+            0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT
+        )
+        val builder = NotificationCompat.Builder(this,"CHANNEL_ID")
+        builder.setContentIntent(contentIntent)
+            .setSmallIcon(android.R.drawable.alert_dark_frame)
+            .setContentTitle("Количество одногруппников в базе")
+            .setContentText(groupmateLen.toString())
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFY_ID, builder.build())
 
     }
 
--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -6,13 +6,16 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.room.Room
@@ -23,17 +26,34 @@ import java.lang.Exception
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var prefs: SharedPreferences
+    private val NOTIFICATION = "NOTIFICATION"
+
     @SuppressLint("WrongThread")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //DB connect
         DBConnect.db = Room.databaseBuilder(
             applicationContext,
             GroupmatesDatabase::class.java,
             "groupmate-db"
         ).allowMainThreadQueries()
             .build()
-        createNotificationChannel()
+        //Сохранение настроек
+        prefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
+
+        val checkBox = findViewById<CheckBox>(R.id.notification_checkBox)
+        val editor = prefs.edit()
+        checkBox.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if (isChecked){
+                editor.putBoolean(NOTIFICATION,checkBox.isChecked).commit()
+                createNotificationChannel()
+            }else{
+                editor.putBoolean(NOTIFICATION,checkBox.isChecked).commit()
+                deleteNotificationChanel()
+            }
+        }
        // DBConnect.db.clearAllTables()
     }
     private fun createNotificationChannel() {
@@ -50,6 +70,27 @@ class MainActivity : AppCompatActivity() {
             notificationManager.createNotificationChannel(channel)
         }
     }
+    private fun deleteNotificationChanel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.deleteNotificationChannel("CHANNEL_ID")
+        }
+    }
+
+//    fun createNotificate(view: View) {
+//        val checkBox = findViewById<CheckBox>(R.id.notification_checkBox)
+//        val editor = prefs.edit()
+//        checkBox.setOnCheckedChangeListener{ buttonView, isChecked ->
+//            if (isChecked){
+//                editor.putBoolean(NOTIFICATION,checkBox.isChecked).commit()
+//                createNotificationChannel()
+//            }else{
+//                editor.putBoolean(NOTIFICATION,checkBox.isChecked).commit()
+//                deleteNotificationChanel()
+//            }
+//        }
+//    }
 
 
     fun addToBase(view: View) {
@@ -72,24 +113,67 @@ class MainActivity : AppCompatActivity() {
         DBConnect.db.groupmatesDAO().deleteAll()
     }
 
-    //Кидать уведомление лучше при переходе в отображение списка из базы
-    fun notification(view: View){
-        val groupmateLen = DBConnect.db.groupmatesDAO().getAllGroupmates().size
-        val NOTIFY_ID: Int = 100;
-        val notificationIntent = Intent(this, MainActivity::class.java)
-        val contentIntent = PendingIntent.getActivity(
-            this,
-            0, notificationIntent,PendingIntent.FLAG_CANCEL_CURRENT
-        )
-        val builder = NotificationCompat.Builder(this,"CHANNEL_ID")
-        builder.setContentIntent(contentIntent)
-            .setSmallIcon(android.R.drawable.alert_dark_frame)
-            .setContentTitle("Количество одногруппников в базе")
-            .setContentText(groupmateLen.toString())
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(NOTIFY_ID, builder.build())
+    override fun onDestroy() {
+        val checkBox = findViewById<CheckBox>(R.id.notification_checkBox)
+        val editor = prefs.edit()
+        editor.putBoolean(NOTIFICATION,checkBox.isChecked).commit()
+        deleteNotificationChanel()
+
+//        checkBox.setOnCheckedChangeListener{ buttonView, isChecked ->
+//            if (isChecked){
+//                println(isChecked)
+//                editor.putBoolean(NOTIFICATION, true).commit()
+//            }else{
+//                println(isChecked)
+//                editor.putBoolean(NOTIFICATION, false).commit()
+//            }
+//        }
+       // println(prefs.getBoolean(NOTIFICATION,true))
+
+        super.onDestroy()
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        val checkBox = findViewById<CheckBox>(R.id.notification_checkBox)
+        if (prefs.contains(NOTIFICATION)) {
+            println("Настройка есть")
+            if (prefs.getBoolean(NOTIFICATION, false)) {
+                checkBox.isChecked = prefs.getBoolean(NOTIFICATION, false)
+                createNotificationChannel()
+                println("Настройка TRUE")
+                // println(prefs.getBoolean(NOTIFICATION,false))}
+            } else {
+                println("Настройка FALSE")
+                checkBox.isChecked = false
+                //deleteNotificationChanel()
+            }
+        } else {
+            println("Настройки нет")
+            checkBox.isChecked = false
+        }
+    }
+
+
+//    //Кидать уведомление лучше при переходе в отображение списка из базы
+//    fun notification(){
+//        val groupmateLen = DBConnect.db.groupmatesDAO().getAllGroupmates().size
+//        val NOTIFY_ID: Int = 100;
+//        val notificationIntent = Intent(this, MainActivity::class.java)
+//        val contentIntent = PendingIntent.getActivity(
+//            this,
+//            0, notificationIntent,PendingIntent.FLAG_CANCEL_CURRENT
+//        )
+//        val builder = NotificationCompat.Builder(this,"CHANNEL_ID")
+//        builder.setContentIntent(contentIntent)
+//            .setSmallIcon(android.R.drawable.alert_dark_frame)
+//            .setContentTitle("Количество одногруппников в базе")
+//            .setContentText(groupmateLen.toString())
+//        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//        notificationManager.notify(NOTIFY_ID, builder.build())
+//
+//    }
 
 
 }
